f670cb7318245c2d55638cc05a71ff4915066490
Paul <84198877+paul9956@users.noreply.github.com>
2022-06-06 23:31:50 +0300

    Update searching_answers.py

--- src/core/searching_answers.py
+++ src/core/searching_answers.py
@@ -1,18 +1,18 @@
-import mesh
+from mesh import get_answers, get_type, get_variant
 from functools import cache
 
 
 @cache
-def get_answers(link: str):
+def get_cdz_answers(link: str):
     try:
         result_answers = []
-        for all_answers in range(20):
-            answers = mesh.get_answers(link)
+        for all_answers in range(len(get_answers(link))):
+            answers = get_answers(link)
             [result_answers.append(answer) for answer in answers if answer not in result_answers]
-#             for answer in answers:
-#                 if answer not in result_answers:
-#                     result_answers.append(answer)
-        print(result_answers)
         return result_answers
     except:
         return '⚠️Хм странно, но я ничего не нашел. Проверь правильность ссылки или нажми 👉/help👈'
+
+    
+def type_test(link: str):
+    return f'тип: {get_type(link)}, №{get_variant(link)}'
