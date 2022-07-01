c43122cd67ce8ae9139e868cd764a29a86f5c076
Paul <84198877+paul9956@users.noreply.github.com>
2022-07-01 03:23:55 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from json import load
-from datetime import datetime
 from time import time
 
 from aiogram import Bot, types
@@ -8,7 +7,7 @@ from aiogram.utils import executor
 
 from core.searching_answers import get_cdz_answers, type_test
 
-with open("src/package/client.json", "r") as telegram_data:
+with open("package/client.json", "r") as telegram_data:
     data = load(telegram_data)
 bot = Bot(token=data["telegram_token"])
 client = Dispatcher(bot)
@@ -19,7 +18,7 @@ admin_id, id_users = [489951151, 857280061], [857280061, 612533221, 489951151, 4
 
 
 def analytics(message: types.Message):
-    with open('src/analytics.txt', '+r') as user_id:
+    with open('analytics.txt', '+r') as user_id:
         if str(message.from_user.id) not in user_id.read():
             user_id.write(f'{message.from_user.id, message.from_user.username}\n')
 
@@ -36,14 +35,14 @@ def post(message: types.Message):
 
 @client.message_handler(commands=["start", "help", "хелп", "помощь", "гайд"])
 async def manual(msg: types.Message):
-    with open("src/package/client_lock.json", "r") as file:
+    with open("package/client_lock.json", "r") as file:
         sys = load(file)
     await msg.answer(f"""👋Привет, {msg.from_user.first_name}.\n
 ❗Для начала работы, отправь мне ссылку на тест и я постараюсь найти ответы.\n📝Пример ссылки: {sys['example']}\n
 ⚠️Если возникла какая-либо проблема, ошибка, баг обратитесь в чат поддержки👨🏼‍🔧: {sys['help_place']}.
 Там Вам помогут с сложившейся ситуацией.🎯\n
 💯Также можешь меня оценить,\n просто написав мне оценку👇\n
-🤖Версия бота: 3️⃣.0️⃣
+🤖Версия бота: {sys['version']}
 🪄Ссылка на бота: {sys['link']}
 📌ВК создателя: {sys['vk']}""")
     analytics(msg)
@@ -51,12 +50,12 @@ async def manual(msg: types.Message):
 
 @client.message_handler(commands=['admin', 'analytics'])
 async def admin(msg: types.Message):
-    with open("src/analytics.txt", "r") as analytic:
+    with open("analytics.txt", "r") as analytic:
         if msg.from_user.id in admin_id:
             counter = 0
             for number, id_user in enumerate(analytic.readlines()):
                 await msg.answer(f'{number + 1}. User: {id_user}')
-                counter += 1
+                counter = number + 1
             await msg.answer(f'🙈Всего пользователей: {counter}')
         else:
             await msg.answer("⚠️Для начала, отправь ссылку на тест, и я попробую его решить.🤡")
@@ -64,20 +63,18 @@ async def admin(msg: types.Message):
 
 @client.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-    if msg.animation or msg.audio or msg.contact or msg.dice or msg.document or msg.location\
-            or msg.photo or msg.poll or msg.sticker or msg.sticker or msg.venue or msg.video\
-            or msg.video_note or msg.voice:
-        await msg.answer("⚠️Для начала, отправь ссылку на тест, и я попробую его решить.🛸")
+    trouble = 'Все хорошо'
     if msg.text.startswith("https://uchebnik.mos.ru"):
         try:
             start_time = time()
             await msg.answer(f"👽Начал решать ({type_test(msg.text)})🔗")
             answers = get_cdz_answers(link=msg.text)
             for task_number, task in enumerate(answers):
-                await msg.answer(f"✏Вопрос №{task_number + 1}: {task[0]}\n\n✅Ответ: {task[1]}")
+                await msg.answer(f"✏️Вопрос №{task_number + 1}: {task[0]}\n\n✅Ответ: {task[1]}")
             await msg.answer(f"⏳Решено за {'%s секунд' % round((time() - start_time), 1)}")
-        except:
+        except Exception as e:
             await msg.answer('⚠️Хм странно, но я ничего не нашел. Проверь правильность ссылки или нажми 👉/help👈')
+            trouble = trouble.replace('Все хорошо', '⚠️{}⚠️'.format(e))
 
     elif 'post all' in msg.text and msg.from_user.id == admin_id[0]:
         post_text = msg.text.replace('post ', '').replace('all ', '')
@@ -93,20 +90,22 @@ async def get_text_messages(msg: types.Message):
     else:
         await msg.answer("⚠️Для начала, отправь ссылку на тест, и я попробую его решить.🛸")
 
-    info = f'Text: {msg.text}\nUser: {msg.from_user.get_user_profile_photos}'
+    info = f"Text: {msg.text}\nОшибка: {trouble}\nUser: id: {msg.from_user.id}, name: {msg.from_user.first_name}," \
+           f" username: {msg.from_user.username}"
     await bot.send_message(admin_id[0], info)
     analytics(msg)
 
 
-@client.message_handler(content_types=['animation', 'audio', 'contact', 'dice', 'document', 'location', 'photo', 'poll', 'sticker',
+@client.message_handler(
+    content_types=['animation', 'audio', 'contact', 'dice', 'document', 'location', 'photo', 'poll', 'sticker',
                    'venue', 'video', 'video_note', 'voice'])
 async def get_text_messages(msg: types.Message):
-    if msg.animation or msg.audio or msg.contact or msg.dice or msg.document or msg.location\
-            or msg.photo or msg.poll or msg.sticker or msg.sticker or msg.venue or msg.video\
-            or msg.video_note or msg.voice:
-        await msg.answer("⚠️Для начала, отправь ссылку на тест, и я попробую его решить.🛸")
+    await msg.answer("⚠️Для начала, отправь ссылку на тест, и я попробую его решить.🛸")
+    info = f"Text: {msg.text}\nUser: id: {msg.from_user.id}, name: {msg.from_user.first_name}," \
+           f" username: {msg.from_user.username}"
+    await bot.send_message(admin_id[0], info)
 
 
 if __name__ == "__main__":
-    print(f"Connected at {datetime.today().strftime('%H:%M, %d-%m-%Y')}")
+    print("Connected    ")
     executor.start_polling(client)
